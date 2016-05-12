912b5564ed77a576a0837f7acf9db04214a37244
bribrink <bribrink@gmail.com>
2016-05-11 22:10:30 -0700

    spiderwebber adjustment for table-vals

--- Spiderweb.php
+++ Spiderweb.php
@@ -10,80 +10,80 @@
 echo "Beginning..";
 
 
-        //configs..
+//configs..
 
-        $user    = 'root';
-        $pass    = '';
-        $host    = 'localhost';
-        $db      = 'tracker';
-        $charset = 'utf8';
+$user    = 'root';
+$pass    = '';
+$host    = 'localhost';
+$db      = 'tracker';
+$charset = 'utf8';
 
-        $pdo = new PDO("mysql:host=$host;dbname=$db;charset=$charset", $user, $pass);
+$pdo = new PDO("mysql:host=$host;dbname=$db;charset=$charset", $user, $pass);
 
-        if($pdo){
-            echo "PDO Connection successfull...";
+if($pdo){
+    echo "PDO Connection successfull...";
+}
+
+
+
+
+
+$dir = __DIR__;
+$keysign1 = '';
+$keysign2 = '_id';//if needed, but optional. set to empty string - double quotes - ''  - , if not used
+$dbname = $db;
+//end config
+
+
+
+$dbvarname = 'Tables_in_'.$dbname;
+$sql = "SHOW FULL TABLES WHERE Table_Type = 'BASE TABLE' ";
+$query = $pdo->prepare($sql);
+$query->execute();
+$tables = $query->fetchAll();
+$dbtables = [];
+$default_cols_to_display = ' <?php '.PHP_EOL.' class Displaycols {'.PHP_EOL.'  '.PHP_EOL;
+
+foreach($tables as $k=>$v){
+    $dbtables[] = $v[$dbvarname];
+}
+$ownedby = [];
+$tablefields = [];
+
+
+
+foreach($dbtables as $t){
+    $sql = "SHOW FULL COLUMNS FROM ".$t;
+    $query = $pdo->prepare($sql);
+    $query->execute();
+    $results = $query->fetchAll();
+
+    $fieldsarray = [];
+    foreach($results as $k=>$v){
+
+        $field = $v['Field'];
+        $fieldtype = (object)['type' => $v['Type'], 'label' => $v['Comment'] ];
+
+        $fieldsarray[$field] = $fieldtype;
+
+        $tmp = [];
+
+        //sets the owned-by array here by checking the is had the keysigns,
+        //indicating that it is a foreign key to another table
+        //remember, strpos returns index of first found string, which is 0! evaluates to FALSE here
+        $testfield = strpos($field, $keysign2);
+        if( $testfield !== FALSE ){
+            $stringsompare = strtolower( str_replace([$keysign1,$keysign2],'',$field) );
+            $ownedby[$stringsompare][] = $t;
         }
+    }
+    $tablefields[$t] = (object) $fieldsarray;
 
+    //a config array for default display columns for the table view
+    $default_cols_to_display .= '       public $'.$t.' = [\'id\', \'name\' ]; '.PHP_EOL.PHP_EOL;
 
-
-
-
-       $dir = __DIR__;
-       $keysign1 = '';
-       $keysign2 = '_id';//if needed, but optional. set to empty string - double quotes - ''  - , if not used
-       $dbname = $db;
-       //end config
-
-
-
-       $dbvarname = 'Tables_in_'.$dbname;
-       $sql = "SHOW FULL TABLES WHERE Table_Type = 'BASE TABLE' ";
-       $query = $pdo->prepare($sql);
-       $query->execute();
-       $tables = $query->fetchAll();
-       $dbtables = [];
-       $default_cols_to_display = ' <?php '.PHP_EOL.' class Displaycols {'.PHP_EOL.'  '.PHP_EOL;
-
-       foreach($tables as $k=>$v){
-           $dbtables[] = $v[$dbvarname];
-       }
-       $ownedby = [];
-       $tablefields = [];
-
-
-
-       foreach($dbtables as $t){
-           $sql = "SHOW FULL COLUMNS FROM ".$t;
-           $query = $pdo->prepare($sql);
-           $query->execute();
-           $results = $query->fetchAll();
-
-           $fieldsarray = [];
-           foreach($results as $k=>$v){
-
-               $field = $v['Field'];
-               $fieldtype = (object)['type' => $v['Type'], 'label' => $v['Comment'] ];
-
-               $fieldsarray[$field] = $fieldtype;
-
-               $tmp = [];
-
-               //sets the owned-by array here by checking the is had the keysigns,
-               //indicating that it is a foreign key to another table
-               //remember, strpos returns index of first found string, which is 0! evaluates to FALSE here
-               $testfield = strpos($field, $keysign2);
-               if( $testfield !== FALSE ){
-                   $stringsompare = strtolower( str_replace([$keysign1,$keysign2],'',$field) );
-                   $ownedby[$stringsompare][] = $t;
-               }
-           }
-           $tablefields[$t] = (object) $fieldsarray;
-
-           //a config array for default display columns for the table view
-           $default_cols_to_display .= '       public $'.$t.' = [\'id\', \'name\' ]; '.PHP_EOL.PHP_EOL;
-
-       }
-        $default_cols_to_display .= '
+}
+$default_cols_to_display .= '
         '.PHP_EOL.'
         } ';
 
@@ -95,28 +95,28 @@ echo "Beginning..";
 
 
 
-       foreach($ownedby as $k=>$v) {
-           $text = '<?php '.PHP_EOL.'namespace Bribrink\Crudkiller\crudkiller;'.PHP_EOL;
-           $tmp1 = ' '.PHP_EOL;
+foreach($ownedby as $k=>$v) {
+    $text = '<?php '.PHP_EOL.'namespace Bribrink\Crudkiller\crudkiller;'.PHP_EOL;
+    $tmp1 = ' '.PHP_EOL;
 
-           //create the classes
-           $text .= 'class '.ucfirst($k).'killer {  '.PHP_EOL.PHP_EOL;
+    //create the classes
+    $text .= 'class '.ucfirst($k).'killer {  '.PHP_EOL.PHP_EOL;
 
 
-           foreach($v as $p):
-           $text .= '
+    foreach($v as $p):
+        $text .= '
            public $'.$p.' = \''.$p.'\';  '.PHP_EOL;
-           $tmp1 .= '
+        $tmp1 .= '
            $this->'.$p.' = self::get( \''.$p.'\', $id );   '.PHP_EOL;
-               endforeach;
+    endforeach;
 
-           $text .= '
+    $text .= '
            public $myself;'.PHP_EOL;
 
-           //create the class
+    //create the class
 
 
-           $text .= '
+    $text .= '
 
 
            function __construct($id=null)
@@ -132,7 +132,7 @@ echo "Beginning..";
 
 
 
-       $text .= '
+    $text .= '
             private static function get($tbl,$id=null){
                 $sql = " SELECT * FROM $tbl ";
                 if(!is_null($id)){
@@ -161,124 +161,127 @@ echo "Beginning..";
 
        }    ';
 
-           $d = $dir.'/crudkiller/';
-           if (!is_dir($d)) {
-               mkdir($d, 0777, true); // true for recursive create
-           }
+    $d = $dir.'/crudkiller/';
+    if (!is_dir($d)) {
+        mkdir($d, 0777, true); // true for recursive create
+    }
 
-           $file = fopen($d.'/'.ucfirst($k).'killer.php','w');
-           fwrite($file,$text);
-           fclose($file);
-
-
-
-       }
+    $file = fopen($d.'/'.ucfirst($k).'killer.php','w');
+    fwrite($file,$text);
+    fclose($file);
 
 
 
-        //table class logic
-        foreach($tablefields as $f=>$v ){
+}
+
+
+
+//table class logic
+foreach($tablefields as $f=>$v ){
 
 
 
 
 
-            $classname = str_replace( '_','', ucfirst($f) ).'table';
-            $text = '';
-            $text_construct_vars = '';
-            $functiontext = '';
-            $text .= '<?php '.PHP_EOL.'namespace Bribrink\Crudkiller\table;'.PHP_EOL;;
-            $text .= '
+    $classname = str_replace( '_','', ucfirst($f) ).'table';
+    $text = '';
+    $text_construct_vars = '';
+    $functiontext = '';
+    $text .= '<?php '.PHP_EOL.'namespace Bribrink\Crudkiller\table;'.PHP_EOL;;
+    $text .= '
                    class '.$classname.' { '.PHP_EOL;
 
-            foreach($v as $n=>$t){
+    foreach($v as $n=>$t){
 
-                $text .= '
+        $text .= '
                         public $'.$n.'; '.PHP_EOL;
 
-                $text_construct_vars .= '
+        $text_construct_vars .= '
                         $this->'.$n.'[\'field\'] = (object)[\'name\'=>\''.$n.'\',\'type\'=>\''.$t->type.'\', \'label\' => \'".$t->label."\' ]; ';
-            }
-            $text .= PHP_EOL;
+    }
+    $text .= PHP_EOL;
 
 
-            if(isset($ownedby[$f])){
+    if(isset($ownedby[$f])){
 
-                foreach($ownedby[$f] as $p):
-                $text .= '
+        foreach($ownedby[$f] as $p):
+            $text .= '
                         public $'.$p.' = \''.$p.'\';  '.PHP_EOL;
-                $tmp1 .= '                       $this->'.$p.' = self::get( \''.$p.'\', $id );   '.PHP_EOL;
+            $tmp1 .= '                       $this->'.$p.' = self::get( \''.$p.'\', $id );   '.PHP_EOL;
 
-                endforeach;
+        endforeach;
 
-                $text .= '
-                        public $myself;'.PHP_EOL.PHP_EOL.
-                        'private $con';
+        $text .= '
+                        public $myself;';
 
-                $functiontext = '
+        $functiontext = '
+
+
                 private  function get($tbl,$id=null){
                     $sql = " SELECT * FROM $tbl ";
                     if(!is_null($id)){
                         $sql .= "  WHERE '.$keysign1.$k.$keysign2.' = ".$id;
                     }
                     return self::connect($sql);
-                }'.PHP_EOL.'
+                }
+
 
                 private  function getme($id=null){
                     $sql = " SELECT * FROM '.$k.' WHERE id = ".$id;
                     $query = $this->pdo->prepare($sql);
                     $query->execute();
                     return $query->fetchObject();
-                }'.PHP_EOL.'
+                }
+
 
                  private  function all($id=null){
                     $sql = " SELECT * FROM '.$k.' ";
                     return self::connect($sql);
-                }'.PHP_EOL.'
+                }
+
 
                 private static function connect($sql){
-                    $query = $this->pdo->prepare($sql);
+                    $query = self::con()->prepare($sql);
                     $query->execute();
                     return $query->fetchAll();
-                }'.PHP_EOL;
+                }';
 
-            }
+    }
 
+    $text .= '
+                   public function __construct($id=null) {' ;
 
+    $text .= $text_construct_vars;
+    $text .= PHP_EOL.$tmp1.PHP_EOL;
 
-            $text .= '
-                   public function __construct($id=null) { '.PHP_EOL;
-                $text .= $text_construct_vars;
-                $text .= PHP_EOL.$tmp1.PHP_EOL;
-
-                $text .= '               $vals = self::getme($id);
+    $text .= '$vals = self::getme($id);
                        foreach($vals as $k=>$v){
                            $this->$k = $v;
                        }'.PHP_EOL.PHP_EOL;
 
-            $text .= '} '.PHP_EOL;
+    $text .= '} '.PHP_EOL;
 
-            $text .= $functiontext;
+    $text .= $functiontext;
 
-            $text .= PHP_EOL.'} '.PHP_EOL.PHP_EOL;
+    $text .= PHP_EOL.'} '.PHP_EOL.PHP_EOL;
 
 
 
-            $d = $dir.'/tables/';
-            if (!is_dir($d)) {
-                mkdir($d, 0777, true); // true for recursive create
-            }
-            $file = fopen($d.'/'.$classname.'.php','w');
-            fwrite($file,$text);
-            fclose($file);
+    $d = $dir.'/tables/';
+    if (!is_dir($d)) {
+        mkdir($d, 0777, true); // true for recursive create
+    }
+    $file = fopen($d.'/'.$classname.'.php','w');
+    fwrite($file,$text);
+    fclose($file);
 
-        }
+}
 
-        //default cols array
-        $d = $dir.'/';
-        $file = fopen($d.'Displaycols.php','w');
-        fwrite($file,$default_cols_to_display);
-        fclose($file);
+//default cols array
+$d = $dir.'/';
+$file = fopen($d.'Displaycols.php','w');
+fwrite($file,$default_cols_to_display);
+fclose($file);
 
 
 
